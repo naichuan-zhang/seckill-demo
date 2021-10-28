c900e756a8d354bd694e88f731e0899250cb9396
znc <naichuanzhang@126.com>
2021-10-28 15:54:18 +0800

    update sql

--- sql/db.sql
+++ sql/db.sql
@@ -16,31 +16,63 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `goods`
+-- Table structure for table `t_goods`
 --
 
-DROP TABLE IF EXISTS `goods`;
+DROP TABLE IF EXISTS `t_goods`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `goods` (
-                         `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
-                         `goods_name` varchar(16) DEFAULT NULL COMMENT '商品名称',
-                         `goods_title` varchar(64) DEFAULT NULL COMMENT '商品标题',
-                         `goods_img` varchar(64) DEFAULT NULL COMMENT '商品图片',
-                         `goods_detail` longtext COMMENT '商品详情',
-                         `goods_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品价格',
-                         `goods_stock` int(11) DEFAULT '0' COMMENT '商品库存，-1为无限制',
-                         PRIMARY KEY (`id`)
+CREATE TABLE `t_goods` (
+                           `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+                           `goods_name` varchar(16) DEFAULT NULL COMMENT '商品名称',
+                           `goods_title` varchar(64) DEFAULT NULL COMMENT '商品标题',
+                           `goods_img` varchar(64) DEFAULT NULL COMMENT '商品图片',
+                           `goods_detail` longtext COMMENT '商品详情',
+                           `goods_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品价格',
+                           `goods_stock` int(11) DEFAULT '0' COMMENT '商品库存，-1为无限制',
+                           PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `goods`
+-- Dumping data for table `t_goods`
 --
 
-LOCK TABLES `goods` WRITE;
-/*!40000 ALTER TABLE `goods` DISABLE KEYS */;
-/*!40000 ALTER TABLE `goods` ENABLE KEYS */;
+LOCK TABLES `t_goods` WRITE;
+/*!40000 ALTER TABLE `t_goods` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_goods` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_order`
+--
+
+DROP TABLE IF EXISTS `t_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_order` (
+                           `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单ID',
+                           `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+                           `goods_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
+                           `delivery_addr_id` bigint(20) DEFAULT NULL COMMENT '收货地址ID',
+                           `goods_name` varchar(16) DEFAULT NULL COMMENT '冗余过来的商品名称',
+                           `goods_count` int(11) DEFAULT '0' COMMENT '商品数量',
+                           `goods_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品单价',
+                           `order_channel` tinyint(4) DEFAULT '0' COMMENT '1pc,2android,3ios',
+                           `status` tinyint(4) DEFAULT '0' COMMENT '订单状态，0新建未支付，1已支付，2已发货，3已收货，4已退款，5已完成',
+                           `create_date` datetime DEFAULT NULL COMMENT '订单创建时间',
+                           `pay_date` datetime DEFAULT NULL COMMENT '订单支付时间',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_order`
+--
+
+LOCK TABLES `t_order` WRITE;
+/*!40000 ALTER TABLE `t_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_order` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -81,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-28 14:06:38
+-- Dump completed on 2021-10-28 15:53:40
